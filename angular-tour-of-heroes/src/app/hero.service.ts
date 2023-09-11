d226626df4e458e23273ae8564adfc4c5b62d62e
cyrtz <cys20030203@gmail.com>
2023-09-11 19:50:59 +0800

    新增一個新英雄的功能

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -54,6 +54,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesURL, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message:string) {
     this.messageService.add(`HeroService: ${message}`);
   }
