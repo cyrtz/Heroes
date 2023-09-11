a3baccca798283fb71b7b6ef83fb7f53c2001855
cyrtz <cys20030203@gmail.com>
2023-09-11 20:16:58 +0800

    刪除英雄的功能

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -61,6 +61,15 @@ export class HeroService {
     );
   }
 
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesURL}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message:string) {
     this.messageService.add(`HeroService: ${message}`);
   }
